0de395350bc1ceb578f56ebf14337971def9cfe7
MD MERAJ ALAM <48945968+mdmeraj1319@users.noreply.github.com>
2020-12-25 16:22:24 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,57 +2,80 @@ import React, { useState } from 'react';
 import './index.css';
 
 const App = () => {
-    const [details,setDetails] = useState({
+    const [fullName, setFullName] = useState({
         fname : "",
         lname : "",
         email : "",
     });
-    const updates = (event) => {
-       const name = event.target.name;
-       const value = event.target.value;
-        setDetails((preValue)=>{
+
+    const inputEvent = (event) =>{
+        console.log(event.target.value);
+        console.log(event.target.name);
+        const value = event.target.value;
+        const name = event.target.name;
+
+        setFullName((preValue)=>{
             if(name === 'fName'){
-                return{
-                   fname : value ,
-                   lname : preValue.lname ,
-                   email : preValue.email ,
+                return {
+                    fname: value ,
+                    lname: preValue.lname,
                 };
             }else if(name === 'lName'){
+                return {
+                    fname: preValue.fname ,
+                    lname: value,
+                };
+            }else if(name === 'eMail'){
                 return{
                     fname : preValue.fname ,
-                    lname : value ,
-                    email : preValue.email,
-                };
-            }else if(name === "eMail"){
-                return{
-                    fname : preValue.fname , 
-                    lname : preValue.lname , 
-                    email : value
+                    lname : preValue.lname ,
+                    email : value ,
                 };
             }
         });
-        
     };
+
     const onSubmits = (event) =>{
         event.preventDefault();
-        alert("from submitted");
-    }
+        alert("form submitted");
+    };
+    
     return(
-        <>
-           <div>
-               <form onSubmit = {onSubmits}>
-                   <div>
-                       <h1> Hello {details.fname} {details.lname} {details.email} </h1>
-                       <input type="text" placeholder="Enter your first Name" name="fName"  onChange={updates} value ={details.fname} ></input>
-                       <input type="text" placeholder="Enter your Second Name" name="lname"  onChange={updates} value={details.lname} ></input>
-                       <input type="text" placeholder="Enter your E-mail" name="eMail"  onChange={updates} value={details.email} ></input>
-                       <button type="submit">Click me 🖕</button>
-                   </div>
-               </form>
-           </div>
-
-
-        </>
+      <>
+          <div className="main_div">
+             <form onSubmit={onSubmits}>
+                <div>
+                    <h1> 
+                        Hello {fullName.fname} {fullName.lname} { fullName.email}
+                    </h1>
+                    <input
+                        type = "text"
+                        placeholder="Enter your name"
+                        name ='fName'
+                        onChange ={inputEvent}
+                        value ={fullName.fname}
+                    />
+                    <br/>
+                    <input
+                        type = "text"
+                        placeholder="Enter your Last name"
+                        name ='lName'
+                        onChange ={inputEvent}
+                        value ={fullName.lname}
+                    />
+                    <br/>
+                    <input
+                        type = "text"
+                        placeholder="Enter your Email"
+                        name ='eMail'
+                        onChange ={inputEvent}
+                        value ={fullName.email}
+                    />
+                    <button type="submit"> Submit Me 👌</button>
+                </div>
+             </form>
+          </div>
+      </>
     );
 }
 
